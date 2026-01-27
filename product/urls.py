396b7bcd93ba9fbc3ebd5b3cdc02d19261dc5b65
Lelushhh <azybekovabidin@gmail.com>
2026-01-27 15:15:16 +0600

    first commit

---
+++ product/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('categories/', category_list),
+    path('categories/<int:id>/', category_detail),
+
+    path('products/', product_list),
+    path('products/<int:id>/', product_detail),
+
+    path('reviews/', review_list),
+    path('reviews/<int:id>/', review_detail),
+]
